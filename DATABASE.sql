baec775850dc59a5765084971d140ee8184c1ce0
aish27 <27aishwarya@gmail.com>
2015-10-30 18:53:20 -0700

    Update the ddl to ensure library has several types of items.

--- DATABASE.sql
+++ DATABASE.sql
@@ -3,33 +3,31 @@ CREATE DATABASE LIBRARYS;
 USE LIBRARYS; 
 
 CREATE TABLE LibraryBranch
-(uID INT,
- LibraryBranchID INT auto_increment,
+( LibraryBranchID INT auto_increment,
  BranchName INT,
- updatedOn timestamp not null on update current_timestamp,
  PRIMARY KEY (LibraryBranchID)
 ) ;
 
-CREATE TABLE BOOK
+CREATE TABLE ITEM
 (
  title VARCHAR(50) NOT NULL,
  author VARCHAR(30),
- BookId INT,
+ ItemId INT,
  Edition VARCHAR(50),
  YearBought INT,
  Category VARCHAR (30),
  LibraryBranchID INT ,
- PRIMARY KEY(BookId),
+ ItemType CHAR(2),
+ PRIMARY KEY(ItemId),
  FOREIGN KEY ( LibraryBranchID) references LibraryBranch(LibraryBranchID)
  );
 
 CREATE TABLE Person
 (PersonId INT AUTO_INCREMENT,
  uNAME VARCHAR(30) NOT NULL,
- age INT,
  UserType VARCHAR (30),
  PreferredBranch INT,
- updatedOn timestamp not null on update current_timestamp,
+ TotalLoansMade INT,
  PRIMARY KEY (PersonId),
  FOREIGN KEY ( PreferredBranch) references LibraryBranch(LibraryBranchID)
 ) ;
@@ -38,21 +36,21 @@ ALTER table Person AUTO_INCREMENT = 1001;
 CREATE TABLE LOAN
 (LoanId INT auto_increment,
  Pid INT,
- Bid INT,
+ Itemid INT,
  loanDate DATE DEFAULT '0000-00-00',
  overdue BOOLEAN DEFAULT FALSE,
  PRIMARY KEY(LoanId),
  FOREIGN KEY (Pid) references Person (PersonId),
- FOREIGN KEY (Bid) references Book(BookId) 
+ FOREIGN KEY (Itemid) references Item(ItemId) 
 ) ;
 
 CREATE TABLE Rating
 (RatingId INT ,
  RatingDate INT,
- BookId INT,
+ ItemId INT,
  PersonId INT,
  Stars BOOLEAN DEFAULT FALSE,
  PRIMARY KEY(RatingId),
  FOREIGN KEY (PersonId) references Person (PersonId),
- FOREIGN KEY (BookId) references BOOK(BookId) 
+ FOREIGN KEY (ItemId) references Item(ItemId) 
 ) ;
