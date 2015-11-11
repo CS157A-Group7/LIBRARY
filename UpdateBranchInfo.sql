083942a3bb7664bad60a01f84478088601b65500
BrendonYim <brendon.sjsu@outlook.com>
2015-11-11 15:25:30 -0800

    Update UpdateBranchInfo.sql

--- UpdateBranchInfo.sql
+++ UpdateBranchInfo.sql
@@ -1,15 +1,18 @@
 -- Users shall be able to update information. [preferred branch] - 11/14
 
+Use Library;
 DROP PROCEDURE IF EXISTS UpdateBranchInfo;
 DELIMITER //
-CREATE PROCEDURE UpdateBranchInfo ( IN BranchInfo int)
+CREATE PROCEDURE UpdateBranchInfo ( IN BranchInfo int, uName varchar(150), PersonId int, out result int)
 BEGIN
 START TRANSACTION;
-	UPDATE person SET PreferredBranch =  BranchInfo Where uNAME= person.uNAME or person.PersonId;
+	UPDATE person SET PreferredBranch =  BranchInfo Where uNAME= person.uNAME or personId = person.personId;
 END //
 DELIMITER ;
 
-UPDATE person SET PreferredBranch =  4 Where uNAME= 'Anne';
+#UPDATE person SET PreferredBranch =  5 Where uNAME= 'Anne';
 
 
-call UpdateBranchInfo(4);
+call UpdateBranchInfo(8, 'Anne',null, @result);
+
+select @result;
