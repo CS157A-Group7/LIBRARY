88a055a006e63e2bb6d89ebdd8421876c9081805
BrendonYim <brendon.sjsu@outlook.com>
2015-11-11 14:46:25 -0800

    Create UpdateBranchInfo.sql
    Please review, Aish!

---
+++ UpdateBranchInfo.sql
@@ -0,0 +1,18 @@
+-- Users shall be able to update information. [preferred branch] - 11/14
+
+
+
+
+DROP PROCEDURE IF EXISTS UpdateBranchInfo;
+DELIMITER //
+CREATE PROCEDURE UpdateBranchInfo ( IN BranchInfo int)
+BEGIN
+START TRANSACTION;
+	UPDATE person SET PreferredBranch =  BranchInfo Where uNAME= person.uNAME or person.PersonId;
+END //
+DELIMITER ;
+
+UPDATE person SET PreferredBranch =  4 Where uNAME= 'Anne';
+
+
+call UpdateBranchInfo(4);
