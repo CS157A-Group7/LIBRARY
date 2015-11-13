c1fcbe5112d89411f3d2fc175de0366ff0fd31d8
aish27 <aishwaryavnkt@yahoo.com>
2015-11-13 13:57:01 -0800

    Updated the SQL query to work. preferredBranch contains integer value not string.

--- UpdateBranchInfo.sql
+++ UpdateBranchInfo.sql
@@ -1,18 +1,4 @@
 -- Users shall be able to update information. [preferred branch] - 11/14
 
-Use Library;
-DROP PROCEDURE IF EXISTS UpdateBranchInfo;
-DELIMITER //
-CREATE PROCEDURE UpdateBranchInfo ( IN BranchInfo int, uName varchar(150), PersonId int)
-BEGIN
-START TRANSACTION;
-	UPDATE person SET PreferredBranch =  BranchInfo Where uNAME= person.uNAME or personId = person.personId;
-END //
-DELIMITER ;
-
-#UPDATE person SET PreferredBranch =  5 Where uNAME= 'Anne';
-
-
-call UpdateBranchInfo(9, 'Anne',null);
-
-select uNAME, PreferredBranch from person;
+update person set PreferredBranch= (select libraryBranchID from librarybranch where BranchName=?) where person.PersonId = ?";
+      
