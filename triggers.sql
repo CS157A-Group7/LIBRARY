31e25f50a4382bc81f0c2a719ee92de903c5d598
aish27 <aishwaryavnkt@yahoo.com>
2015-11-30 23:39:11 -0800

    Fixed one of the triggers.

--- triggers.sql
+++ triggers.sql
@@ -1,30 +1,26 @@
-use library;
-select * from person;
+-- when the item id of a book is changed, make similar changes in related tables.
 DROP TRIGGER IF EXISTS UpdateItemidTrigger;
 delimiter //
 CREATE TRIGGER UpdateItemidTrigger
 AFTER UPDATE ON item
 FOR EACH ROW
 BEGIN
-UPDATE LOAN SET Itemid = NEW.ItemId
-WHERE ItemId = OLD.ItemId;
-UPDATE RATING SET Itemid = NEW.ItemId
-WHERE ItemId = OLD.ItemId;
+UPDATE LOAN SET Itemid = NEW.ItemId WHERE ItemId = OLD.ItemId;
+UPDATE RATING SET Itemid = NEW.ItemId WHERE ItemId = OLD.ItemId;
 END//
 delimiter ;
 
 DROP TRIGGER IF EXISTS InsertTrigger;
 
-
-select * from loan;
+-- when a loan is made, update the number of copies as well as the total loans made by user.
 DROP TRIGGER IF EXISTS UpdateUserTotalLoans;
 delimiter //
 CREATE TRIGGER UpdateUserTotalLoans
 AFTER INSERT ON loan
 FOR EACH ROW
 BEGIN
-UPDATE person SET TotalLoansMade = TotalLoansMade+1
-WHERE Personid = new.pid;
+UPDATE person SET TotalLoansMade = TotalLoansMade+1 WHERE Personid = new.pid;
+update item set copies = copies -1 where item.ItemId = new.itemid;
 
 END//
 delimiter ;
