ae2c9084e413672d2baadc21e1c102d40f77326f
aish27 <aishwaryavnkt@yahoo.com>
2015-12-01 08:46:04 -0800

    Added new query involving set operations

---
+++ newQuery.sql
@@ -0,0 +1,8 @@
+-- Find all books that are in a audio/video media format. 
+select distinct title,author,edition,itemtype from item where itemtype like "%ebook%" 
+ union   
+ select distinct title,author,edition,itemtype from item where itemtype like "%audio%" 
+ union
+ select  distinct title,author,edition,itemtype from item where itemtype like"%cd%"
+ union  
+ distinct select title,author,edition,itemtype from item where itemtype like "%dvd%" ; 
