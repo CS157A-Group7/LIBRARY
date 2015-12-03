81f3506f51ac61f263c2fa31d1814ea70369c61a
BrendonYim <brendon.sjsu@outlook.com>
2015-12-03 07:24:27 -0800

    Update newQuery.sql

--- newQuery.sql
+++ newQuery.sql
@@ -1,4 +1,4 @@
--- Find all books that are in a audio/video media format. 
+-- 15. Find all books that are in a audio/video media format. 
 select distinct title,author,edition,itemtype from item where itemtype like "%ebook%" 
  union   
  select distinct title,author,edition,itemtype from item where itemtype like "%audio%" 
